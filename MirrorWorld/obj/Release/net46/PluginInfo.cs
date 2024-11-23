92f216f14ae0b3812793023b3aa89ef804812020
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-11-23 13:57:18 -0300

    clean everything up

--- MirrorWorld/obj/Release/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MirrorWorld
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "MirrorWorld";
-        public const string PLUGIN_NAME = "MirrorWorld";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
